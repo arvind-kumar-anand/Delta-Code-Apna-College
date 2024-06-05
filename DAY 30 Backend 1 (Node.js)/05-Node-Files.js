53fa41694eed9948efd4964e3490a403d5e126e5
Arvind Kumar Anand <arvindanand.0015@gmail.com>
2024-06-05 19:20:48 +0530

    day 30 is updated

--- DAY 30 Backend 1 (Node.js)/05-Node-Files.js
+++ DAY 30 Backend 1 (Node.js)/05-Node-Files.js
@@ -1,16 +1,6 @@
-// let n = 5;
-// for (let i = 0; i < n; i++) {
-//     console.log("number is :" + i);
-// }
-
-// console.log("Arvind");
-console.log(process.argv);
-
-let args = process.argv;
-// for (let i = 2; i < args.length; i++) {
-//     console.log("Hello : " + args[i]);
-// }
-
-for (let i = 0; i < args.length; i++) {
-    console.log("Hello : " + args[i]);
-}
+const math = require("./07-Export-in-Files");
+console.log(math);
+console.log(math.sum(5, 7));
+console.log(math.mul(5, 7));
+console.log(math.PI);
+console.log(math.g);
---
+++ DAY 30 Backend 1 (Node.js)/07-Export-in-Files.js
@@ -0,0 +1,34 @@
+// const sum = (a, b) => a + b;
+// const mul = (a, b) => a + b;
+// const PI = 3.14;
+// const g = 9.8;
+
+// module.exports.sum = (a, b) => a + b;
+// module.exports.mul = (a, b) => a + b;
+// module.exports.PI = 3.14;
+// module.exports.g = 9.8;
+
+exports.sum = (a, b) => a + b;
+exports.mul = (a, b) => a + b;
+exports.PI = 3.14;
+exports.g = 9.8;
+
+// exports = 9.8; //ERROR
+
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     PI: PI,
+//     g: g,
+// }
+
+// module.exports = obj;
+
+
+// module.exports = {
+//     sum: sum,
+//     mul: mul,
+//     PI: PI,
+//     g: g,
+// }
+
