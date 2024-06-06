0e301bf46ecd354403136974bda6db1c9d5f0f15
Arvind Kumar Anand <arvindanand.0015@gmail.com>
2024-06-06 15:35:34 +0530

    day 30 with self package json made and installed is updated

--- DAY 30 Backend 1 (Node.js)/10-Installing-Packages/app.js
+++ DAY 30 Backend 1 (Node.js)/10-Installing-Packages/app.js
@@ -1,6 +1,6 @@
 const figlet = require("figlet");
 
-figlet("Hello World!!", function (err, data) {
+figlet("Arvind Kumar Anand", function (err, data) {
     if (err) {
         console.log("Something went wrong...");
         console.dir(err);
