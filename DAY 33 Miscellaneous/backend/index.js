6c2665ae8f776764f93893589e5414d39cf8f13d
Arvind Kumar Anand <arvindanand.0015@gmail.com>
2024-06-14 18:42:00 +0530

    updated

--- DAY 33 Miscellaneous/backend/index.js
+++ DAY 33 Miscellaneous/backend/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/register", (req, res) => {
     // console.log("standard GET response");
-    res.send("standard GET response");
+    let { user, password } = req.query;
+    res.send(`standa rd GET response. Welcome ${user}`);
 })
 
 app.post("/register", (req, res) => {
     // console.log("standard GET response");
-    res.send("standard POST response");
+    console.log(req.body);
+    res.send(`standard POST response.`);
 })
 
 app.listen(port, () => {
