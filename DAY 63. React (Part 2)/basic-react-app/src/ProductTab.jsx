1e647d4dc38d73d7bd04731ababf43af9ed13a8f
Arvind Kumar Anand <arvindanand.0015@gmail.com>
2024-09-24 15:24:22 +0530

    Updated

--- DAY 63. React (Part 2)/basic-react-app/src/ProductTab.jsx
+++ DAY 63. React (Part 2)/basic-react-app/src/ProductTab.jsx
@@ -1,6 +1,22 @@
 import Products from "./Products.jsx";
 
+// LECTURE 1,2,3
+// function ProductTab() {
+//   let option1 = ["tech", "durable", "cheap"];
+//   // let option1 = [<li>tech</li>, <li>durable</li>, <li>cheap</li>];
+//   // let option2 = { a: "service", b: "capability", c: "Resolution" };
+//   return (
+//     <div>
+//       <Products title="mobile" price={20000} features1={option1} />
+//       <Products title="laptop" price={40000} />
+//       <Products title="charger" price={20} />
+//     </div>
+//   );
+// }
+
+//04. Conditionals
 function ProductTab() {
+  // let option1 = ["tech", "durable", "cheap"];
   return (
     <div>
       <Products title="mobile" price={20000} />
--- DAY 63. React (Part 2)/basic-react-app/src/Products.jsx
+++ DAY 63. React (Part 2)/basic-react-app/src/Products.jsx
@@ -1,16 +1,31 @@
 import "./Products.css";
 
-// function Products({ title, price=1 }) { //DEFAULT VALUE
+// LECTURE 1,2,3
+// function Products({ title, price, features1 }) {
+//   // console.log(probs);
+//   // console.log(probs.title);
+
+//   return (
+//     <div className="Products">
+//       <h3>{title}</h3>
+//       <p>It's price is {price}</p>
+//       <p>
+//         {features1.map((feature) => (
+//           <li>{feature}</li>
+//         ))}
+//       </p>
+//     </div>
+//   );
+// }
+
+//04. Conditionals
 function Products({ title, price }) {
-  // console.log(probs);
-  // console.log(probs.title);
-  let name = "arvind";
-  let options = ["tech", "durable", ""];
   return (
     <div className="Products">
-      {/* <h3>{probs.title}</h3> */}
       <h3>{title}</h3>
-      <h3>Price is Rs. {price}</h3>
+      <p>It's price is {price}</p>
+      {price > 20000 ? <p>discount of 5%</p> : <null />}
+      {/* {price > 20000 && <p>discount of 5%</p>} */}
     </div>
   );
 }
