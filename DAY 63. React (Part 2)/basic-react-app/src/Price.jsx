7b4dbbe00adaf6dfaabbca103a5db5c373f046d6
Arvind Kumar Anand <arvindanand.0015@gmail.com>
2024-09-25 19:14:05 +0530

    Updated

---
+++ DAY 63. React (Part 2)/basic-react-app/src/Price.jsx
@@ -0,0 +1,25 @@
+function Price({ oldPrice, newPrice }) {
+  let oldStyles = {
+    textDecoration: "line-through",
+  };
+  let newStyles = {
+    fontWeight: "bold",
+  };
+  let styles = {
+    backgroundColor: "gold",
+    borderBottomLeftRadius: "16px",
+    borderBottomRightRadius: "16px",
+    height: "40px",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  return (
+    <div style={styles}>
+      <span style={oldStyles}> &#8377;{oldPrice}</span>
+      &nbsp; &nbsp; &nbsp;
+      <span style={newStyles}> &#8377;{newPrice}</span>
+    </div>
+  );
+}
+
+export default Price;
