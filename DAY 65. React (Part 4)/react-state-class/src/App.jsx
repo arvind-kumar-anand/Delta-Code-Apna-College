5b4d38b0e80ad277da761a7f9caa8824a9ea01cb
Arvind Kumar Anand <arvindanand.0015@gmail.com>
2024-09-30 00:34:08 +0530

    Updated

--- DAY 65. React (Part 4)/react-state-class/src/App.jsx
+++ DAY 65. React (Part 4)/react-state-class/src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import Counter from "./Counter.jsx";
-// import LikeButton from "./LikeButton.jsx";
+import LudoBoard from "./LudoBoard.jsx";
 
 export default function App() {
   return (
     <>
-      <h3>State in React</h3>
-      <Counter />
-      {/* <LikeButton /> */}
+      <LudoBoard />
     </>
   );
 }
---
+++ DAY 65. React (Part 4)/react-state-class/src/LudoBoard.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Game Begins!</h1>
+      <div>
+        <p>Blue Moves =</p>
+        <button>+1</button>
+
+        <p>Yello Moves =</p>
+        <button>+1</button>
+
+        <p>Green Moves =</p>
+        <button>+1</button>
+
+        <p>Red Moves =</p>
+        <button>+1</button>
+      </div>
+    </div>
+  );
+}
