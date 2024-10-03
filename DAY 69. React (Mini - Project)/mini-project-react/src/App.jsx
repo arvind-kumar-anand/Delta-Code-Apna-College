bee301b9a6716670e8c61f470faae41e7cd4f180
Arvind Kumar Anand <arvindanand.0015@gmail.com>
2024-10-03 23:44:00 +0530

    Updated

--- DAY 69. React (Mini - Project)/mini-project-react/src/App.jsx
+++ DAY 69. React (Mini - Project)/mini-project-react/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import Alert from "@mui/material/Alert";
+
+let handleContained = () => {
+  console.log("Button Clicked");
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button
+        variant="contained"
+        onClick={handleContained}
+        size="small"
+        startIcon={<DeleteIcon />}
+      >
+        Delete
+      </Button>
+
+      <Button
+        variant="contained"
+        color="success"
+        size="medium"
+        onClick={handleContained}
+        endIcon={<SendIcon />}
+      >
+        Send
+      </Button>
+
+      <Button
+        variant="outlined"
+        color="error"
+        onClick={handleContained}
+        size="large"
+      >
+        Error
+      </Button>
+
+      <Alert severity="success">This is a success Alert.</Alert>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
